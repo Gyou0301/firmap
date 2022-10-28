9ff0e0faab22c2b8169d312e6a636cc33b94da7a
Gyou0301 <gotoyudai0301@gmail.com>
2022-10-29 02:11:34 +0900

    Update home.js

--- home.js
+++ home.js
@@ -1,1 +1,10 @@
-
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('sample'), {
+        center: {
+            lat: 34.7019399,
+            lng: 135.51002519999997
+        },
+        zoom: 19
+    });
+}
